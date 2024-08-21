fdb5751ea2524bfb4750d397701ada37fda3bfa8
alok-003 <alokchawat@gmail.com>
2024-08-21 19:52:38 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,24 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Implemention of Operators
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter No. for a:"<<endl;
+    cin>>a;
+    cout<<"Enter No. for b:"<<endl;
+    cin>>b;
+    c = a + b;
+    cout<<"The sum is:"<<c;
+}
+
+
+/*Output:
+Enter No. for a:
+5
+Enter No. for b:
+2
+The sum is:7*/
